a608c355920cf761329b7e206e548913818ee7d9
Jason Rafferty <Jason0x55@gmail.com>
2018-06-25 18:35:48 -0600

    Initial version of landing page.

--- app/src/main/java/com/toldcalculator/android/tc/MainActivity.java
+++ app/src/main/java/com/toldcalculator/android/tc/MainActivity.java
@@ -1,11 +1,8 @@
 package com.toldcalculator.android.tc;
 
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
-import android.support.v4.app.FragmentManager;
-import android.view.View;
 import android.support.design.widget.NavigationView;
+import android.support.v4.app.FragmentManager;
 import android.support.v4.view.GravityCompat;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBarDrawerToggle;
@@ -23,6 +20,10 @@ public class MainActivity extends AppCompatActivity
     setContentView(R.layout.activity_main);
     Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
     setSupportActionBar(toolbar);
+
+    FragmentManager fragmentManager = getSupportFragmentManager();
+    fragmentManager.beginTransaction().add(R.id.main_container, new NewFlightFragment()).commit();
+
 
     DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
     ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
--- app/src/main/java/com/toldcalculator/android/tc/NewFlightFragment.java
+++ app/src/main/java/com/toldcalculator/android/tc/NewFlightFragment.java
@@ -26,14 +26,22 @@ public class NewFlightFragment extends Fragment {
       Bundle savedInstanceState) {
     // Inflate the layout for this fragment
     View view = inflater.inflate(R.layout.fragment_new_flight, container, false);
+    String[] profiles = new String[]{"N1234", "N2345", "N3456"};
+    String[] airports = new String[]{"KABQ", "KAPA", "KPHX"};
 
-    Spinner spinner = view.findViewById(R.id.aircraft_profiles);
-    String[] items = new String[]{"N1234", "N2345", "N3456"};
+    Spinner spinner = (Spinner) view.findViewById(R.id.aircraft_profiles);
 
-    ArrayAdapter<String> adapter = new ArrayAdapter<String>(this.getActivity(), android.R.layout.simple_spinner_item, items);
+    ArrayAdapter<String> adapter = new ArrayAdapter<String>(getActivity(), R.layout.spinner_item, profiles);
 
     adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
     spinner.setAdapter(adapter);
+
+//    spinner = (Spinner) view.findViewById(R.id.airport_list);
+//    adapter = new ArrayAdapter<String>(getActivity(), R.layout.spinner_item, airports);
+//
+//    adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+//    spinner.setAdapter(adapter);
+
     return view;
   }
 
